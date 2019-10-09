f810bfbdb3d0c92f6ee3158c76dc8cfbd54930a1
Hedzr Yeh <hedzrz@gmail.com>
2019-10-09 23:42:26 +0800

    bumped to v1.1.0
    - +nacl, +plan9
    
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- doc.go
+++ doc.go
@@ -6,31 +6,10 @@ package logex
 
 const (
 	// AppName const
-	AppName = "logex" // main app-name
+	AppName = "logex"
 	// Version const
-	Version = "1.0.3" // version name
+	Version = "1.1.0"
 	// VersionInt const
-	VersionInt = 0x010003 // using as
+	VersionInt = 0x010100
 )
 
-// func GetApiPrefix() string {
-// 	return "/api/v1"
-// }
-//
-// func GetApiVersion() string {
-// 	return "v1"
-// }
-
-// func GetAppExitCh() chan bool {
-// 	return appExitCh
-// }
-//
-// var appExitCh = make(chan bool)
-
-// func IsProd() bool {
-// 	switch vxconf.GetStringR("runmode", "devel") {
-// 	case "prod", "production":
-// 		return true
-// 	}
-// 	return false
-// }
